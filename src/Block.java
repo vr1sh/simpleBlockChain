4b60bc0ecfdaf372bd7f77267619df1801efa799
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-06-13 14:36:37 +0530

    Block

--- src/Block.java
+++ src/Block.java
@@ -1,4 +1,8 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.logging.Level;
 
 public class Block {
 
@@ -7,11 +11,35 @@ public class Block {
     private String data;
     private long timeStamp;
 
-    public Block(String data,String previousHash ) {
+    public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calcHash();
     }
+
+    public static String applySHA(String input) {
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String calcHash() {
+        String hash = applySHA(previousHash + Long.toString(timeStamp) + data);
+        return hash;
+    }
+
 }
 
 
