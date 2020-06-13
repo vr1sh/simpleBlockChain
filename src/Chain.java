5f700375db9e7e0c68078e3a02f62941abe14ef7
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-06-13 15:51:18 +0530

    Block

--- src/Chain.java
+++ src/Chain.java
@@ -3,16 +3,19 @@ import java.util.Arrays;
 
 public class Chain {
 
+    static int i = 1;
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
     public static void main(String[] args) {
         //add our blocks to the blockchain ArrayList:
-        blockchain.add(new Block("This is the first block", "0"));
-        blockchain.add(new Block("This is the second block", blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("This is the third block", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("genesis block", "0"));
+        blockchain.add(new Block("second block", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("third block", blockchain.get(blockchain.size()-1).hash));
 
         for(Block block : blockchain) {
-            System.out.println(block.hash);
+            System.out.println("Hash of block number" + " " + i + " " + block.hash);
+            System.out.println();
+            i++;
         }
 
     }
